5f7ce0d9b75ccf66e31a81c1c6882bd22cef6a25
Aboobacker sabith <aboobackersabith150@gmail.com>
2024-09-03 10:41:35 +0530

    first commit

---
+++ days.js
@@ -0,0 +1,26 @@
+var a = 8
+
+if(a == 1){
+    console.log("Sunday")
+}
+else if(a == 2){
+    console.log("Monday")
+}
+else if(a == 3){
+    console.log("Tuesday")
+}
+else if(a == 4){
+    console.log("Wednesday")
+}
+else if(a == 5){
+    console.log("Thursday")
+}
+else if(a == 6){
+    console.log("Friday")
+}
+else if(a == 7){
+    console.log("Saturday")
+}
+else{
+    console.log("WRONG INFORMATION")
+}
